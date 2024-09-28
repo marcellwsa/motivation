f8d01d0f329e3e801fc78326e956bc849017ff5c
Marcell Wendling <mwsa.pe@gmail.com>
2024-09-28 18:28:53 -0300

    Conclusão do Layout

--- app/src/main/java/devandroid/isa/motivation/MainActivity.kt
+++ app/src/main/java/devandroid/isa/motivation/MainActivity.kt
@@ -1,20 +1,35 @@
 package devandroid.isa.motivation
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import devandroid.isa.motivation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        //supportActionBar?.show()
+
+
+        binding.btnNovaFrase.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_nova_frase) {
+            var s = ""
         }
     }
 }
