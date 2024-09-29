c9359fa7566edb39f29a436f3ec233038f315981
Marcell Wendling <mwsa.pe@gmail.com>
2024-09-29 14:19:05 -0300

    Navegacao entre activities

--- app/src/main/java/devandroid/isa/motivation/UserActivity.kt
+++ app/src/main/java/devandroid/isa/motivation/UserActivity.kt
@@ -1,14 +1,16 @@
 package devandroid.isa.motivation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import devandroid.isa.motivation.databinding.ActivityUserBinding
 
-class UserActivity : AppCompatActivity(), View.OnClickListener  {
+class UserActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityUserBinding
 
@@ -27,7 +29,17 @@ class UserActivity : AppCompatActivity(), View.OnClickListener  {
 
     override fun onClick(v: View) {
         if (v.id == R.id.btn_save) {
+            handleSave()
+        }
+    }
 
+    private fun handleSave() {
+        val name = binding.edtName.text.toString()
+        if (name != "") {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            Toast.makeText(this, R.string.validation_mandatory_name, Toast.LENGTH_SHORT).show()
         }
     }
 }
